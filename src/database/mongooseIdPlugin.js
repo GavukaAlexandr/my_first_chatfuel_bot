684da012b099acc2fbc30996096b5f40690d554b
Oleksandr Gavuka <oleksandr.gavuka@masterofcode.com>
2018-02-09 11:01:37 +0200

    added Postgres image and config

--- src/database/mongooseIdPlugin.js
+++
@@ -1,8 +0,0 @@
-export default function mongooseIdPlugin(schema, options) {
-    schema.options.toJSON.transform = function (doc, ret, options) {
-      delete ret._id; // Remove internal _id property before exposing it as JSON.
-      return ret;
-    };
-    schema.set('toJSON', schema.options.toJSON);
-  }
-  
